7107638a9b76c82bc7628a394ad9d1760130ffc4
maddambie <Joycerivera0812@gmail.com>
2025-04-27 16:44:36 +0800

    -.-

--- src/main/java/com/appdev/SCT/controller/UserController.java
+++ src/main/java/com/appdev/SCT/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -87,10 +89,14 @@ public class UserController {
 
 
 		    
-		    @PostMapping("/login")
-		    public String showloginForm(@RequestParam String hiddenValue, Model model) {
+			@RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+		    public String showloginForm(@RequestParam(required = false) String hiddenValue, Model model) {
 		        model.addAttribute("user", new User());
-		        model.addAttribute("hiddenValue", hiddenValue);
+		       
+		        if(hiddenValue == null || hiddenValue.trim().isEmpty()) {
+		        	hiddenValue = "Student";
+		        }
+		        model.addAttribute("hiddenValue", hiddenValue);		
 		        return "login";
 		    }
 		    
